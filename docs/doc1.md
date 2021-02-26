4c2356915f87931167ba3f9a6b06bf6f102311c3
ranyishere <rany.r.tith@gmail.com>
2021-02-26 14:17:07 -0800

    initial commit

---
+++ docs/doc1.md
@@ -0,0 +1,35 @@
+---
+id: doc1
+title: Getting Started
+sidebar_label: Getting Started
+slug: /
+---
+
+> Logic is the systematic study of valid rules of inference, i.e. the relations that lead to the acceptance of one proposition (the conclusion) on the basis of a set of other propositions (premises). More broadly, logic is the analysis and appraisal of arguments. - Wikipedia
+
+## Purpose
+
+This document was created to help mathematicians, logicians, and computer scientists as a reference. This document is a **highly opinionated way** to present mathematics in a formatted and rigorous way.
+
+Each entry has:
+* Definition
+* Example
+* Reference
+
+such that no exposition or historical background are presented. Such information can be provided elsewhere and should be provided by references to get more information. The motivation for this
+format is to become an easier reference for working mathematicians. The general idea is to mirror API references that we see in programming libraries.
+
+For example see:
+* https://www.w3schools.com/html/
+* https://api.rubyonrails.org/classes/ActiveJob/Arguments.html
+* https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/Appflow/Client.html
+
+## Contribution
+
+Contribution can be made by sending a pull request and following community guidelines.
+
+I would advise you to take this course:
+> https://developers.google.com/tech-writing/overview
+
+## References
+1. https://en.wikipedia.org/wiki/Logic
---
+++ src/pages/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Easy to Use',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+	Logic was designed by humans for humans. It is a framework for
+	    reasoning about the world.
+      </>
+    ),
+  },
+  {
+    title: 'Prove statements',
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+	How can you guarantee you are doing anything right? Logic provides axioms
+	in combination with your natural built reasoning to underestand the world.
+      </>
+    ),
+  },
+  {
+    title: 'Powerful',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+	With using any flavor of logic
+	you can study any system in a rigorous and formal manner.
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
